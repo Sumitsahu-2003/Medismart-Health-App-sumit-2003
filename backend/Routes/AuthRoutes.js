bb6f691dccf71037b64a8670b2ef25f235cfd468
Sumit Kumar Sahu <sumitsahu89845@gmail.com>
2025-11-22 13:31:23 +0530

    Update AuthRoutes.js

--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const authCtrl = require('../controllers/authController');
+const authCtrl = require('../controllers/AuthController');
 
 router.post('/register',authCtrl.register);
 router.post('/admin/login',authCtrl.adminLogin);
 
 router.get('/dashboard',authCtrl.dashboard,authMiddleware);
 router.post('/patient/login',authCtrl.patientLogin);
+
 module.exports = router;
